df9b2f8610e274bca150340e391a831c9c1a634a
Hero-Xero <omarahmedasaad@gmail.com>
2025-09-06 09:24:30 +0300

    added index.js content and added dependencies

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose"); 
+const logger = require("./middleware/logger");
 
-// ----------------------
-// 2. Register Routes
-// ----------------------
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/categories", require("./routes/categoryRoutes"));
-app.use("/api/cart", require("./routes/cartRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+app.use(logger);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something wrong :(");
+});
 
 
+mongoose
+  .connect(
+    "mongodb+srv://EcoMeanDB:EcoMeanDB2025@ecommerce-mean-db.9ac0d4z.mongodb.net/"
+  )
+  .then(() => {
+    console.log(" DB CONNECTED Successfully ");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/users", require("./routes/userRoutes"));
+// app.use("/api/products", require("./routes/productRoutes"));
+// app.use("/api/categories", require("./routes/categoryRoutes"));
+// app.use("/api/cart", require("./routes/cartRoutes"));
+// app.use("/api/orders", require("./routes/orderRoutes"));
+
+
+let port = 5000;
+app.listen(port, () => {
+  console.log(`${port} works!`);
+});
