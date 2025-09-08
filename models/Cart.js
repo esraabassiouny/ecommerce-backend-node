0ef8756f0e955b6f4e872865e49f238195e803bb
esraabassiouny <127542864+esraabassiouny@users.noreply.github.com>
2025-09-08 05:30:03 +0300

    Add Cart and Order Schema

--- models/Cart.js
+++ models/Cart.js
@@ -5,3 +5,26 @@
 // - items: array of { productId, quantity, price }
 // - totalPrice: Number
 // - updatedAt
+const mongoose = require('mongoose')
+
+const cartItemSchema = mongoose.Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true, min: 1, default: 1 },  
+    price: { type: Number, required: true },  
+}, { _id: false });
+
+const cartSchema = mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },             
+  items: [cartItemSchema], 
+  totalPrice: { type: Number, required: true, default: 0 },             
+},{ timestamps: true }
+);
+
+
+cartSchema.pre("save", function (next) {
+  this.totalPrice = this.items.reduce((acc, item) => acc + item.price, 0);
+  next();
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = Cart;
--- models/Order.js
+++ models/Order.js
@@ -8,3 +8,68 @@
 // - paymentMethod: enum ["COD", "Credit Card"]
 // - status: enum ["Pending", "Shipped", "Delivered"], default "Pending"
 // - timestamps
+const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  price: {
+    type: Number, 
+    required: true
+  }
+}, { _id: false });
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [orderItemSchema],
+  shippingAddress: {
+    type: String
+    // label: { type: String, default: "Home" },
+    // country: { type: String, required: true },
+    // street: { type: String, required: true },
+    // buildingNameOrNo: { type: String, required: true },
+    // floor: { type: Number, required: true },
+    // apartmentNo: { type: Number, required: true },
+    // city: { type: String, required: true },
+    // postalCode: String,
+    },
+  paymentMethod: {
+    type: String,
+    enum: ['COD', 'Credit Card', 'PayPal'],
+    default: 'COD'
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+    default: 'Pending'
+  }
+}, { timestamps: true });
+
+orderSchema.pre("save", function (next) {
+  this.totalPrice = this.items.reduce((acc, item) => acc + item.price, 0);
+  this.totalPrice += this.shippingPrice;
+  next();
+});
+
+module.exports = mongoose.model('Order', orderSchema);
